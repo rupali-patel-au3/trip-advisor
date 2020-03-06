324595c7d4fc127320ebc454042b427f6e0be2ee
rupali-patel-au3 <rupalipatel60@gmail.com>
2020-03-06 14:28:54 +0530

    tripadvisor

--- trip-advisor/trip-advisor/src/components/cards/card.js
+++ trip-advisor/trip-advisor/src/components/cards/card.js
@@ -10,7 +10,7 @@ import {
   CardBody
 } from "reactstrap";
 
-const Example = props => {
+const Box = props => {
   return (
     <div className="container d-flex">
       <div className="col-md-8 ">
@@ -99,6 +99,48 @@ const Example = props => {
             </CardBody>
           </Card>
         </CardDeck>
+        <br />
+        <CardDeck>
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in Dubai</CardTitle>
+
+              <Button>Read more...</Button>
+            </CardBody>
+          </Card>
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in Dubai</CardTitle>
+
+              <Button>Read more...</Button>
+            </CardBody>
+          </Card>
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in karnataka</CardTitle>
+
+              <Button>Read more...</Button>
+            </CardBody>
+          </Card>
+        </CardDeck>
       </div>
       <br />
       <div className="col-md-3">
@@ -135,4 +177,4 @@ const Example = props => {
   );
 };
 
-export default Example;
+export default Box;
--- trip-advisor/trip-advisor/src/pages/HolidayPackages/holidayPackages.js
+++ trip-advisor/trip-advisor/src/pages/HolidayPackages/holidayPackages.js
@@ -1,2 +1,105 @@
+import React from "react";
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardDeck,
+  CardBody
+} from "reactstrap";
 
+const HolidayPackages = props => {
+  return (
+    <div className="container d-flex">
+      <div className="col-md-8 ">
+        <br />
+        <CardDeck>
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in Dubai</CardTitle>
 
+              <Button>Read more...</Button>
+            </CardBody>
+          </Card>
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in Dubai</CardTitle>
+
+              <Button>Read more...</Button>
+            </CardBody>
+          </Card>
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in karnataka</CardTitle>
+
+              <Button>Read more...</Button>
+            </CardBody>
+          </Card>
+        </CardDeck>
+        <br />
+        <CardDeck>
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in Goa</CardTitle>
+
+              <Button>Read more...</Button>
+            </CardBody>
+          </Card>
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in Delhi</CardTitle>
+
+              <Button>Read more...</Button>
+            </CardBody>
+          </Card>
+
+          <Card className="col-md-4">
+            <CardImg
+              top
+              width="100%"
+              src="https://lh3.googleusercontent.com/proxy/ZC4A3T5OIIta1FHu4zH7rYeYtLOFY7Ue1suESfscUWKwz2xaomokRtIwl9Y-0X7hL8T7WUvDdMgIkNSlJdztii5CGvVIYy3TrBVIDT2IIMtDyABi3R4m0cfWKneUIllxJwnATjpg6KGBnJ1OZObR8DYk5Gl0JdbOlFBOMOcIvOVTmRcPADUAijJF6N0L"
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>Top hotels in Dubai</CardTitle>
+
+              <Button>Read more..</Button>
+            </CardBody>
+          </Card>
+        </CardDeck>
+      </div>
+    </div>
+  );
+};
+
+export default HolidayPackages;
